98fd71353086602be13274314e161b9c4c63b4e3
taskpul <romtietz@gmail.com>
2026-04-22 14:22:16 +0200

    Revert "Hide pro upsell notices and remove locked options from builder UI"

--- assets/js/uip/uibuilder/block-settings.js
+++ assets/js/uip/uibuilder/block-settings.js
@@ -1199,10 +1199,7 @@ export default {
       if (blockOptionsIndex < 0) return [];
       return allBlockSettings[blockOptionsIndex].options;
     },
-     returnVisibleBlockOptions() {
-      return this.returnBlockOptions.filter((option) => this.componentExists(option.componentName) && this.isAvailable(option));
-    },
- },
+  },
   methods: {
     /**
      * Opens block settings
@@ -1447,12 +1444,17 @@ export default {
             </ToggleSection>
             <!-- End general settings -->
             
-            <div v-if="uiTemplate.proActivated" class="uip-border-top"></div>
+            <div class="uip-border-top"></div>
             
             <!-- Query loop  -->
-            <ToggleSection v-if="uiTemplate.proActivated" :title="strings.queryLoop">
+            <ToggleSection :title="strings.queryLoop">
             
-              <QueryBuilder :block="block" :value="returnBlockQuerySettings" :returnData="(d)=>{ block.query.settings = d}"/>
+              <QueryBuilder v-if="uiTemplate.proActivated" :block="block" :value="returnBlockQuerySettings" :returnData="(d)=>{ block.query.settings = d}"/>
+              
+              <div class="uip-grid-col-1-3" v-else>
+                <div class="uip-text-muted uip-flex uip-flex-center uip-text-s"><span>{{ strings.queryLoop }}</span></div>
+                <div class="uip-padding-xxs uip-border-rounder uip-background-green-wash uip-text-s">{{strings.proOption}}</div>
+              </div>
               
             </ToggleSection>
             <!-- End query -->
@@ -1467,14 +1469,14 @@ export default {
             </ToggleSection>
             <!-- End Responsive -->
             
-            <div class="uip-border-top" v-if="returnVisibleBlockOptions.length"></div>
+            <div class="uip-border-top" v-if="returnBlockOptions.length"></div>
             
             <!-- Block options  -->
-            <ToggleSection :title="strings.options" :startOpen="true" v-if="returnVisibleBlockOptions.length">
+            <ToggleSection :title="strings.options" :startOpen="true" v-if="returnBlockOptions.length">
                   
                   <div class="uip-flex uip-flex-column uip-row-gap-xs">
                   
-                    <template v-for="option in returnVisibleBlockOptions">
+                    <template v-for="option in returnBlockOptions">
                       
                       <div :class="optionFullWidth(option) ? 'uip-flex uip-flex-column uip-row-gap-xxs' : 'uip-grid-col-1-3'">
                       
@@ -1494,10 +1496,15 @@ export default {
                           
                         <div class="uip-flex uip-flex-center uip-w-100p">
                         
-                          <component :is="option.componentName" 
+                          <component v-if="componentExists(option.componentName) && isAvailable(option)" 
+                          :is="option.componentName"
                           v-bind="option"
                           :value="returnBlockSettingValue(option)"
                           :returnData="(data)=>handleBlockSettingUpdate(option, data)"/>
+                          
+                          <div v-else class="uip-padding-xxs uip-border-rounder uip-background-green-wash uip-text-s">
+                             {{strings.proOption}}
+                          </div>
                         </div>
                         
                       </div>
